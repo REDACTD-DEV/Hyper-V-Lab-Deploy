e3dbe7e4991fb2e9caf77b59ee3a28cb2344f6f0
[REDACTED] <22129641+REDACTD-DEV@users.noreply.github.com>
2022-10-13 10:52:06 +1100

    Update WEB01-PostInstall.ps1

--- WEB01-PostInstall.ps1
+++ WEB01-PostInstall.ps1
@@ -1,3 +1,3 @@
 #Install IIS role
-Write-Host "Install IIS role" -ForegroundColor Blue -BackgroundColor Black
-Install-WindowsFeature -name "Web-Server" -IncludeAllSubFeature -IncludeManagementTools
+Write-Host "Install IIS role" -ForegroundColor Blue -BackgroundColor Black | Out-Null
+Install-WindowsFeature -name "Web-Server" -IncludeAllSubFeature -IncludeManagementTools | Out-Null
