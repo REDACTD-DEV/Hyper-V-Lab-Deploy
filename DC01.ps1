207c4fd33c07fc47d7295e2c00fbb36169d1b89f
[REDACTED] <22129641+REDACTD-DEV@users.noreply.github.com>
2022-10-13 10:55:12 +1100

    Update DC01.ps1

--- DC01.ps1
+++ DC01.ps1
@@ -1,5 +1,5 @@
 #Disable IPV6
-Get-NetAdapterBinding | Where-Object ComponentID -eq 'ms_tcpip6' | Disable-NetAdapterBinding 
+Get-NetAdapterBinding | Where-Object ComponentID -eq 'ms_tcpip6' | Disable-NetAdapterBinding | Out-Null
 
 #Rename network adapter inside VM
 Write-Host "Rename network adapter inside VM" -ForegroundColor Blue -BackgroundColor Black
@@ -14,16 +14,16 @@ $Params = @{
     DefaultGateway = $using:GW01.IP
     PrefixLength = $using:Prefix
 }
-Get-NetAdapter -Name "Internal" | New-NetIPAddress @Params 
+Get-NetAdapter -Name "Internal" | New-NetIPAddress @Params | Out-Null
 
 Write-Host "Set DNS" -ForegroundColor Blue -BackgroundColor Black
 #Configure DNS Settings
-Get-NetAdapter -Name "Internal" | Set-DNSClientServerAddress -ServerAddresses $using:DC01.IP   
+Get-NetAdapter -Name "Internal" | Set-DNSClientServerAddress -ServerAddresses $using:DC01.IP  | Out-Null  
 
 #Install AD DS server role
 Write-Host "Install AD DS Server Role" -ForegroundColor Blue -BackgroundColor Black
-Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools 
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools  | Out-Null
 
 #Configure server as a domain controller
 Write-Host "Configure server as a domain controller" -ForegroundColor Blue -BackgroundColor Black
-Install-ADDSForest -DomainName $using:Domain -DomainNetBIOSName $using:DomainNetBIOSName -InstallDNS -Force -SafeModeAdministratorPassword (ConvertTo-SecureString $using:Password -AsPlainText -Force) -WarningAction SilentlyContinue 
+Install-ADDSForest -DomainName $using:Domain -DomainNetBIOSName $using:DomainNetBIOSName -InstallDNS -Force -SafeModeAdministratorPassword (ConvertTo-SecureString $using:Password -AsPlainText -Force) -WarningAction SilentlyContinue  | Out-Null
